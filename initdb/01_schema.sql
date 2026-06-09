b96d3afd373deafee553b19e5855772a2d9b1af4
Weidemann <felix.weidemann@getec.de>
2026-06-09 13:10:41 +0200

    generalize initdb folder

--- initdb/01_schema.sql
+++ initdb/01_schema.sql
@@ -1,1 +1,15 @@
-CREATE DATABASE Employee;
+CREATE DATABASE Flowster;
+
+USE Flowster;
+
+CREATE TABLE tblEmployees (
+    id SERIAL PRIMARY KEY,
+    SamAccountName VARCHAR(50) NOT NULL,
+    Guest INT,
+    AD_EmployeeId VARCHAR(100),
+    Surname VARCHAR(50) NOT NULL,
+    GivenName VARCHAR(50) NOT NULL,
+    changed DATE,
+    isActive INT,
+    ExpirationDate DATE
+)
